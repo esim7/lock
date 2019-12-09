d403a44ddfda255d76098c145f43c913b0c10472
esim7 <makishev@yahoo.com>
2019-12-10 02:55:17 +0600

    загрузил lock

---
+++ BankSystem/Account.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankSystem
+{
+    public class Account
+    {
+        public delegate void MyAccount(string message);
+        public event MyAccount Notification;
+
+        public string Name { get; set; }
+        public string AccountNumber { get; set; }
+        public int Cash { get; set; }
+
+        public Account()
+        {
+            Notification += ShowMessage;
+        }
+        private static void ShowMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void AddMoney(object money)
+        {
+            Cash += (int)money;
+            Notification?.Invoke($"На счет {this.Name} номер счета {this.AccountNumber} поступило {(int)money}");
+        }
+        public void WithDrawMoney(object money)
+        {
+            Cash -= (int)money;
+            Notification?.Invoke($"Со счета {this.Name} номер счета {this.AccountNumber} списано {(int)money}");
+        }
+    }
+}
---
+++ BankSystem/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace BankSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object syncObject = new object();
+            
+        BlockingCollection<Account> accounts = new BlockingCollection<Account>()
+        {
+            new Account
+            {
+                Name = "Пупкин А. В.",
+                AccountNumber = "KZ74155898RB0000345",
+                Cash = 10000
+            },
+            new Account
+            {
+                Name = "Сидоров О. С.",
+                AccountNumber = "KZ71114528RB0000345",
+                Cash = 10000
+            },
+            new Account
+            {
+                Name = "Федоров Ф. В.",
+                AccountNumber = "KZ78876966RB0000345",
+                Cash = 10000
+            },
+            new Account
+            {
+                Name = "Бугарин У. В.",
+                AccountNumber = "KZ78985532RB0000345",
+                Cash = 10000
+            },
+            new Account
+            {
+            Name = "Петров А. Л.",
+            AccountNumber = "KZ77855654RB0000345",
+            Cash = 10000
+            },
+        };
+        for(int i = 0; i < 100; i++)
+        {
+            lock (accounts)
+            {
+                if (i % 2 == 0)
+                {
+                    for (int j = 0; j < accounts.Count; j++)
+                    {
+                        ThreadPool.QueueUserWorkItem(accounts.ElementAt(j).AddMoney, 100);
+                    }
+                }
+                else
+                {
+                    for (int j = 0; j < accounts.Count; j++)
+                    {
+                        ThreadPool.QueueUserWorkItem(accounts.ElementAt(j).WithDrawMoney, 100);
+                    }
+                }
+                Thread.Sleep(5);
+            }
+        }
+
+        foreach(var account in accounts)
+        {
+             Console.WriteLine(account.Name + " - " + account.Cash);
+        }
+        Console.ReadLine();
+        }
+    }
+}
